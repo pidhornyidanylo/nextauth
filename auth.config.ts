f897a257d0af1d6a0654a632d201fe9bb226803d
Danylo Pidhornyi <105637978+pidhornyidanylo@users.noreply.github.com>
2024-03-17 12:29:48 +0200

    Add files via upload

---
+++ auth.config.ts
@@ -0,0 +1,6 @@
+// Here's what i did to make a custom login page instead of default NextAuth
+// export const authConfig = {
+//     pages: {
+//         signIn: '/login' // Specify the path to your custom sign-in page
+//     }
+// }
